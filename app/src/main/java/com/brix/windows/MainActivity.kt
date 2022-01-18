d831113e18ee35997856bc6e4884137e569a35ba
Brixter Porras <brixterporras@gmail.com>
2022-01-18 23:11:25 +0800

    Fix Issue on AddSub From not showing Text from the start

--- app/src/main/java/com/brix/windows/MainActivity.kt
+++ app/src/main/java/com/brix/windows/MainActivity.kt
@@ -9,7 +9,6 @@ import android.app.DatePickerDialog
 import android.os.Build
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
@@ -19,26 +18,30 @@ import java.time.Period
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import android.widget.RadioGroup
+
+import android.widget.RadioButton
+import kotlin.collections.ArrayList
+import android.widget.ArrayAdapter
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var mYear: Int = 0
-    private var mMonth: Int = 0
-    private var mDay: Int = 0
-
-    private val TAG = "MainActivity"
-
     private lateinit var txtDiffFrom: TextView
     private lateinit var txtDiffTo: TextView
+    private lateinit var txtDiffDay: TextView
     private lateinit var txtDiffResult: TextView
     private lateinit var txtAddSubFrom: TextView
-
+    private lateinit var txtAddSubResult: TextView
     private lateinit var spinner: Spinner
-
     private lateinit var layoutDiff: LinearLayout
     private lateinit var layoutAddSub: LinearLayout
-
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var spinnerYear: Spinner
+    private lateinit var spinnerMonth: Spinner
+    private lateinit var spinnerDay: Spinner
+    private lateinit var radioButtonAdd: RadioButton
+    private lateinit var radioButtonSub: RadioButton
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,31 +49,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         txtDiffFrom = findViewById(R.id.txtDiffFrom)
         txtDiffTo = findViewById(R.id.txtDiffTo)
+        txtDiffDay = findViewById(R.id.txtDiffDay)
         txtDiffResult = findViewById(R.id.txtDiffResult)
         txtAddSubFrom = findViewById(R.id.txtAddSubFrom)
-
+        txtAddSubResult = findViewById(R.id.txtAddSubResult)
         spinner = findViewById(R.id.spinner)
-
         layoutDiff = findViewById(R.id.layoutDiff)
         layoutAddSub = findViewById(R.id.layoutAddSub)
-
-
-//      setting value to calendar
-        val c = Calendar.getInstance()
-        mYear = c[Calendar.YEAR]
-        mMonth = c[Calendar.MONTH]
-        mDay = c[Calendar.DAY_OF_MONTH]
+        radioGroup = findViewById(R.id.radioGroup)
+        spinnerYear = findViewById(R.id.spinnerYear)
+        spinnerMonth = findViewById(R.id.spinnerMonth)
+        spinnerDay = findViewById(R.id.spinnerDay)
+        radioButtonAdd = findViewById(R.id.radioButtonAdd)
+        radioButtonSub = findViewById(R.id.radioButtonSub)
 
 //      setting current date to the text view
         txtDiffFrom.text = getCurrentDate()
         txtDiffTo.text = getCurrentDate()
         txtAddSubFrom.text = getCurrentDate()
 
+        setSpinnerItem()
         onDataChange()
         getDifference()
     }
 
     private fun onDataChange() {
+        val year = spinnerYear.selectedItem as Int
+        val month = spinnerMonth.selectedItem as Int
+        val day = spinnerDay.selectedItem as Int
+
+
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 if (spinner.selectedItem.toString() == "Difference between dates") {
@@ -80,6 +88,40 @@ class MainActivity : AppCompatActivity() {
                 if (spinner.selectedItem.toString() == "Add or subtract days") {
                     layoutAddSub.visibility = View.VISIBLE
                     layoutDiff.visibility = View.GONE
+
+                    if (radioButtonAdd.isChecked) {
+                        spinnerAddDate(spinnerYear)
+                        spinnerAddDate(spinnerMonth)
+                        spinnerAddDate(spinnerDay)
+                    } else {
+                        spinnerSubDate(spinnerYear)
+                        spinnerSubDate(spinnerMonth)
+                        spinnerSubDate(spinnerDay)
+                    }
+//                  Text Change Listener Sub and Add from Text
+                    txtAddSubFrom.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(
+                            p0: CharSequence?,
+                            p1: Int,
+                            p2: Int,
+                            p3: Int
+                        ) {
+                        }
+
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        }
+
+                        @RequiresApi(Build.VERSION_CODES.O)
+                        override fun afterTextChanged(p0: Editable?) {
+                            if (radioButtonAdd.isChecked) {
+                                addDate(year, month, day)
+                            } else {
+                                subDate(year, month, day)
+                            }
+                        }
+
+                    })
+
                 }
             }
 
@@ -88,22 +130,46 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        txtDiffFrom.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+        textChangeListener(txtDiffFrom)
+        textChangeListener(txtDiffTo)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onRadioButtonClicked(view: View) {
+        val year = spinnerYear.selectedItem as Int
+        val month = spinnerMonth.selectedItem as Int
+        val day = spinnerDay.selectedItem as Int
+
+        if (view.id == radioButtonAdd.id) {
+            addDate(year, month, day)
+        } else {
+            subDate(year, month, day)
+        }
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radioButtonAdd ->
+                    if (checked) {
+                        spinnerAddDate(spinnerYear)
+                        spinnerAddDate(spinnerMonth)
+                        spinnerAddDate(spinnerDay)
+                    }
+                R.id.radioButtonSub ->
+                    if (checked) {
+                        spinnerSubDate(spinnerYear)
+                        spinnerSubDate(spinnerMonth)
+                        spinnerSubDate(spinnerDay)
+                    }
             }
+        }
+    }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            @RequiresApi(Build.VERSION_CODES.O)
-            override fun afterTextChanged(p0: Editable?) {
-                getDifference()
-            }
-
-        })
-        txtDiffTo.addTextChangedListener(object : TextWatcher {
+    private fun textChangeListener(textView: TextView) {
+        textView.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -129,9 +195,13 @@ class MainActivity : AppCompatActivity() {
         val period = Period.between(from, to)
         val weeks = ChronoUnit.WEEKS.between(from, to)
         val days = ChronoUnit.DAYS.between(from, to)
-        val result =
-            period.years.toString() + " years \n" + period.months.toString() + " months \n" + weeks.toString() + " weeks \n" + days + " days"
+        val result = period.years.toString() + " Year".pluralize(period.years) + "\n" +
+                period.months.toString() + " Month".pluralize(period.months) + "\n" +
+                weeks.toString() + " Week".pluralize(weeks.toInt()) + "\n" +
+                period.days.toString() + " Day".pluralize(period.days)
+
         txtDiffResult.text = result
+        txtDiffDay.text = days.toString() + " Day".pluralize(days.toInt())
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -191,6 +261,15 @@ class MainActivity : AppCompatActivity() {
         return Calendar.getInstance().time
     }
 
+    //  plural singular
+    private fun String.pluralize(count: Int): String? {
+        return if (count > 1) {
+            this + 's'
+        } else {
+            this
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun formatTextView(textView: TextView): LocalDate {
         //      format text to locale date
@@ -199,8 +278,100 @@ class MainActivity : AppCompatActivity() {
         return LocalDate.parse(toFormat, firstApiFormat)
     }
 
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun onClickAddSub(view: android.view.View) {
         datePicker(txtAddSubFrom).show()
     }
+
+    private fun setSpinnerItem() {
+        val arrayList: ArrayList<Int?> = ArrayList()
+
+        for (i in 0..999) {
+            arrayList.add(i)
+        }
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayList)
+        // Set layout to use when the list of choices appear
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        // Set Adapter to Spinner
+        spinnerYear.adapter = arrayAdapter
+        spinnerMonth.adapter = arrayAdapter
+        spinnerDay.adapter = arrayAdapter
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun addDate(year: Int, month: Int, day: Int) {
+//        getting date from text view ang converting tong date
+        val localDate = formatTextView(txtAddSubFrom)
+        val defaultZoneId: ZoneId = ZoneId.systemDefault()
+        val date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant())
+
+        val c = Calendar.getInstance()
+        c.time = date
+
+        // manipulate date
+        c.add(Calendar.YEAR, year)
+        c.add(Calendar.MONTH, month)
+        c.add(Calendar.DATE, day)
+//      setting and formatting Date for result
+        val currentDatePlusOne = c.time
+        txtAddSubResult.text = currentDatePlusOne.toString("EEEE, dd MMMM yyyy")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun subDate(year: Int, month: Int, day: Int) {
+//        getting date from text view ang converting tong date
+        val localDate = formatTextView(txtAddSubFrom)
+        val defaultZoneId: ZoneId = ZoneId.systemDefault()
+        val date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant())
+
+        val c = Calendar.getInstance()
+        c.time = date
+
+        // manipulate date
+        c.add(Calendar.YEAR, -year)
+        c.add(Calendar.MONTH, -month)
+        c.add(Calendar.DATE, -day)
+//      setting and formatting Date for result
+        val currentDatePlusOne = c.time
+        txtAddSubResult.text = currentDatePlusOne.toString("EEEE, dd MMMM yyyy")
+    }
+
+    private fun spinnerAddDate(spinner: Spinner) {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val year = spinnerYear.selectedItem as Int
+                val month = spinnerMonth.selectedItem as Int
+                val day = spinnerDay.selectedItem as Int
+
+                addDate(year, month, day)
+            }
+
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+    }
+
+    private fun spinnerSubDate(spinner: Spinner) {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val year = spinnerYear.selectedItem as Int
+                val month = spinnerMonth.selectedItem as Int
+                val day = spinnerDay.selectedItem as Int
+
+                subDate(year, month, day)
+            }
+
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+    }
+
 }
